4bc8f21124250510620d5d35c5c6d1d2d92d5956
Nathan Abraham <nabraham@gmail.com>
2017-12-15 12:38:27 -0500

    generator has modulo

--- 15_generators.py
+++ 15_generators.py
@@ -1,23 +1,21 @@
-def generator(factor, maxx=2147483647):
+def generator(factor, modulus=-1, maxx=2147483647):
     def next(prev):
-        return (prev*factor) % maxx
+        nxt = (prev*factor) % maxx
+        if modulus > 0:
+            while nxt % modulus != 0:
+                nxt = (nxt * factor) % maxx
+        return nxt
     return next
 
 
-def main(a, b, a_mult=-1, b_mult=-1, N=40000000, a_fact=16807, b_fact=48271):
-    genA = generator(a_fact)
-    genB = generator(b_fact)
+def main(a, b, a_mod=-1, b_mod=-1, N=40000000, a_fact=16807, b_fact=48271):
+    genA = generator(a_fact, a_mod)
+    genB = generator(b_fact, b_mod)
     match = 0
     mask = (0xFF << 8) + 0xFF
     for i in range(N):
         a = genA(a)
         b = genB(b)
-        if a_mult > 0:
-            while a % a_mult != 0:
-                a = genA(a)
-            while b % b_mult != 0:
-                b = genB(b)
-
         match += [0, 1][(mask & a) == (mask & b)]
     return match
 
@@ -32,5 +30,3 @@ if __name__ == '__main__':
 
     #PART 2
     print(main(634,301,4,8,5000000))
-
-
