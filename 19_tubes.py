7900078d719165ec72c2719b9227ee21d756b66b
Nathan Abraham <nabraham@gmail.com>
2017-12-19 11:11:15 -0500

    19 - tubes

---
+++ 19_tubes.py
@@ -0,0 +1,72 @@
+import string
+import logging
+
+logger = logging.getLogger('tubes')
+
+def print_maze(m, n=40):
+    for row in m[:n]:
+        logger.warn(''.join(row[:n]))
+
+
+def off_maze(p,m):
+    return p[0] < 0 or p[0] >= len(m) or p[1] < 0 or p[1] >= len(m[p[0]]) 
+
+
+def turn(pos, direction, maze):
+    if direction[0] == 0:
+        if pos[0] > 0 and maze[pos[0] - 1][pos[1]] != ' ':
+            logger.warn('Turning North')
+            return (-1,0)
+        else:
+            logger.warn('Turning South')
+            return (1,0)
+    else:
+        if pos[1] > 0 and maze[pos[0]][pos[1] - 1] != ' ':
+            logger.warn('Turning West')
+            return (0,-1)
+        else:
+            logger.warn('Turning East')
+            return (0,1)
+
+
+
+def main(maze):
+    print_maze(maze)
+    pos = (0, maze[0].index('|'))
+    direction = (1,0)
+    accum = []
+    steps = 0
+    logger.warn('Position: %s, Direction: %s' % (pos, direction))
+    while True:
+        steps += 1
+        pos = (pos[0] + direction[0], pos[1] + direction[1])
+        if off_maze(pos, maze):
+            logger.warn('off the maze')
+            break
+        else:
+            logger.warn('Position: %s, Direction: %s' % (pos, direction))
+            curr = maze[pos[0]][pos[1]]
+            if curr in '-|':
+                pass
+            elif curr == '+':
+                direction = turn(pos, direction, maze)
+            elif curr in string.ascii_uppercase:
+                logger.warn('accumulating "%s"'% curr)
+                accum += curr
+            else:
+                logger.warn('off the path')
+                break
+    return ''.join(accum), steps
+
+
+def run(filename):
+    with open(filename) as f:
+        maze = [list(line.strip('\n')) for line in f.readlines()]
+        logger.error('\n%s\n%s' % (filename, '-'*len(filename)))
+        return main(maze)
+
+
+if __name__ == '__main__':
+    logger.error(run('data/19_test.txt'))
+    logger.setLevel(logging.ERROR)
+    logger.error(run('data/19.txt'))
