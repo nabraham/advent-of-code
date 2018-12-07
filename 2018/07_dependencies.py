91d033f1ff517ed27055f54058645759ce8baf0b
Nathan Abraham <nabraham@gmail.com>
2018-12-07 13:43:38 -0500

    day7 pt 2

--- 2018/07_dependencies.py
+++ 2018/07_dependencies.py
@@ -1,10 +1,40 @@
 import aoc_utils
 import re
 from collections import defaultdict, namedtuple
+from functools import reduce
 from pprint import pprint
 
 step_rx = re.compile(r'Step ([A-Z]) must be finished before step ([A-Z]) can begin')
 Step = namedtuple('Step',['start', 'stop'])
+Task = namedtuple('Task',['name', 'finish_time'])
+FREE = Task('.', -1)
+
+def finish(char, tick, offset):
+    return tick + ord(char) - ord('A') + 1 + offset
+
+
+class Worker:
+    def __init__(self, id):
+        self.id = id
+        self.task = FREE
+
+    def assign(self, task_id, tick, offset):
+        self.task = Task(task_id, finish(task_id, tick, offset))
+
+    def free(self):
+        return self.task.name == '.'
+
+    def process(self, tick):
+        if not self.free() and tick >= self.task.finish_time:
+            t = self.task.name
+            self.task = FREE
+            return t
+        return None
+
+    def __repr__(self):
+        return '%s - %s' % (self.id, self.task)
+
+
 
 def get_steps(lines):
     steps = []
@@ -22,10 +52,6 @@ def get_first(queue, depends, walk):
 
 def walk_graph(g, depends, queue):
     walk = []
-
-    pprint(g)
-    pprint(depends)
-
     while len(queue) > 0:
         first = get_first(queue, depends, walk)
         queue.remove(first)
@@ -37,10 +63,66 @@ def walk_graph(g, depends, queue):
     return ''.join(walk)
 
 
-        
+def still_working(workers):
+    return any(map(lambda x: not x.free(), workers))
+
+
+def any_free(workers):
+    return any(map(lambda x: x.free(), workers))
+
+def in_progress(workers):
+    return set(map(lambda x: x.task.name, workers)).difference(set(['.']))
+
+
+def assign_tasks(graph, walk, depends, queue, workers, tick, offset):
+    should_continue = True
+    while should_continue and len(queue) > 0 and any_free(workers):
+        first = get_first(queue, depends, walk)
+        if first != None:
+            queue.remove(first)
+            w = list(filter(lambda x: x.free(), workers))[0]
+            w.assign(first, tick, offset)
+        else:
+            should_continue = False
+    return queue
+
+
+def add_edges(queue, finished, graph, walk, workers):
+    if len(finished) > 0:
+        all_edges = [graph[f] for f in finished]
+        uniq_new = list(set(reduce(lambda x,y: x + y, all_edges)))
+        new_edges = list(filter(lambda x: x not in walk and x not in in_progress(workers) and x not in queue, uniq_new))
+        return sorted(queue + new_edges)
+    return queue
+
+def print_state(tick, workers, walk, queue):
+    print('%d: %s' % (tick, '\t'.join(
+        [w.task.name for w in workers] + 
+        [''.join(walk)] + 
+        ['<%s>' % ','.join(queue)])))
+    
+    
+
+def walk_with_workers(graph, depends, queue, n_workers, offset):
+    tick = 0
+    workers = [Worker(i) for i in range(1,n_workers + 1)]
+    walk = []
+    while len(queue) > 0 or still_working(workers):
+        processed = map(lambda x: x.process(tick), workers)
+        finished = list(filter(lambda x: x != None, processed))
+        queue = add_edges(queue, finished, graph, walk, workers)
+        walk += finished
+        queue = assign_tasks(graph, walk, depends, queue, workers, tick, offset)
+        #print_state(tick, workers, walk, queue)
+        tick += 1
+    return tick - 1, ''.join(walk)
 
 
 def part1(lines):
+    graph, depends, queue = build_start(lines)
+    return walk_graph(graph, depends, queue)
+
+def build_start(lines):
     start_set = set([])
     stop_set = set([])
     graph = defaultdict(list)
@@ -50,21 +132,22 @@ def part1(lines):
         stop_set.add(s.stop)
         graph[s.start].append(s.stop)
         depends[s.stop].append(s.start)
-    return walk_graph(graph, depends, sorted(list(start_set.difference(stop_set))))
-
-
-
+    return graph, depends, sorted(list(start_set.difference(stop_set)))
 
 
 def part2(lines):
-    return None
+    graph, depends, queue = build_start(lines)
+    n_workers = [2, 5][len(lines) > 10]
+    offset = [0, 60][len(lines) > 10]
+    return walk_with_workers(graph, depends, queue, n_workers, offset)
+
 
 
 def run(filename):
     print(aoc_utils.file_header(filename))
     lines = aoc_utils.get_input(filename)
     pprint(part1(lines))
-    #pprint(part2(lines))
+    pprint(part2(lines))
 
 
 if __name__ == '__main__':
